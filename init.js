11d983af8f6a9e23d91d6e0449f8d0f08ca6b0fe
Danil Kolumbet <kolumbetko@gmail.com>
2020-05-03 19:26:44 +0300

    Add random Color

--- init.js
+++ init.js
@@ -7,9 +7,16 @@ let height = (canvas.height = window.innerHeight);
 
 const ctx = canvas.getContext("2d");
 
+function randomColor() {
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+    return `rgb(${r},${g},${b},1)`;
+}
+
 let pos = new Vector(30, 30);
 let radius = 20;
-let color = "#000";
+let color = randomColor();
 const GRAVITY = new Vector(0, 1);
 
 function frame() {
@@ -19,6 +26,7 @@ function frame() {
 
     ctx.beginPath();
     ctx.arc(pos.x, pos.y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = color;
     ctx.fill();
     requestAnimationFrame(frame);
 }
