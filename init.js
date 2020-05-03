6a76fb37441fcb41251fe2b79d495f0a137d6030
Danil Kolumbet <kolumbetko@gmail.com>
2020-05-03 18:56:35 +0300

    Draw a point

--- init.js
+++ init.js
@@ -1,4 +1,12 @@
+"use strict";
+
 const canvas = document.querySelector("#Canvas");
 
 let width = (canvas.width = window.innerWidth);
 let height = (canvas.height = window.innerHeight);
+
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(width / 2, height / 2, 20, 0, Math.PI * 2);
+ctx.fill();
