3d23cec0a5abae914a313d2ac714d06e91df899d
Danil Kolumbet <kolumbetko@gmail.com>
2020-05-11 17:16:16 +0300

    Fix connecting alredy connected points

--- init.js
+++ init.js
@@ -68,7 +68,18 @@ const keydownHandler = function(e) {
             for (let j = i + 1; j < selectedPoints.length; j++) {
                 const p1 = selectedPoints[i];
                 const p2 = selectedPoints[j];
-                lines.push(new Line(p1, p2));
+                let found = false;
+                lines.map((l) => {
+                    if (
+                        (l.p1 === p1 && l.p2 === p2) ||
+                        (l.p1 === p2 && l.p2 === p1)
+                    ) {
+                        found = true;
+                    }
+                });
+                if (found === false) {
+                    lines.push(new Line(p1, p2));
+                }
             }
         }
         render();
