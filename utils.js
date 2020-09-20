afaf7450134201a410888d171e107cd642f03eee
Danil Kolumbet <kolumbetko@gmail.com>
2020-09-21 00:05:04 +0300

    Move action functions from frame to utils

--- utils.js
+++ utils.js
@@ -21,3 +21,67 @@ function distToSegmentSquared(p, v, w) {
 function clamp(n, min, max) {
     return Math.max(Math.min(n, max), min);
 }
+
+function createNewPoint(connected = false) {
+    let newPos = Input.downPos.copy();
+    if (activePoint) {
+        if (Input.ctrl || Input.gridSnapOnCreate) {
+            let diff = Input.downPos.sub(activePoint.pos).scale(1 / cellSize);
+            diff.set(Math.round(diff.x), Math.round(diff.y));
+            if (diff.length() < 1) {
+                return;
+            }
+            newPos = activePoint.pos.add(diff.scale(cellSize));
+        }
+    }
+    const newPoint = new Point(newPos);
+    points.push(newPoint);
+    if (activePoint && (connected || Input.createConnected))
+        lines.push(new Line(newPoint, activePoint));
+    selectedPoints.push(newPoint);
+    activePoint = newPoint;
+}
+
+function connectSelectedPoints() {
+    for (let i = 0; i < selectedPoints.length; i++) {
+        for (let j = i + 1; j < selectedPoints.length; j++) {
+            const p1 = selectedPoints[i];
+            const p2 = selectedPoints[j];
+            let found = false;
+            lines.map((l) => {
+                if (
+                    (l.p1 === p1 && l.p2 === p2) ||
+                    (l.p1 === p2 && l.p2 === p1)
+                ) {
+                    found = true;
+                }
+            });
+            if (found === false) {
+                lines.push(new Line(p1, p2));
+            }
+        }
+    }
+}
+
+const toggleHidden = (x) =>
+    (x.color = x.color === "transparent" ? x.originalColor : "transparent");
+function hide() {
+    selectedPoints.map(toggleHidden);
+    selectedLines.map(toggleHidden);
+}
+
+function unhide() {
+    points.map((p) => (p.color = p.originalColor));
+    lines.map((l) => (l.color = l.originalColor));
+}
+
+function deleteSelected() {
+    lines = lines.filter(
+        (l) =>
+            selectedPoints.indexOf(l.p1) < 0 && selectedPoints.indexOf(l.p2) < 0
+    );
+    points = points.filter((p) => selectedPoints.indexOf(p) < 0);
+    selectedPoints.length = 0;
+    lines = lines.filter((l) => selectedLines.indexOf(l) < 0);
+    selectedLines.length = 0;
+}
